c3ca6011c0967edbaf4ada13c6daea98673673f0
macbook <pvthanh38@gmail.com>
2016-02-15 19:32:14 +0700

    thanh commit

--- server.php
+++ server.php
@@ -1,6 +1,6 @@
 <?php
 
-/***
+/****
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
@@ -19,3 +19,4 @@ if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
 }
 
 require_once __DIR__.'/public/index.php';
+/****
